4ef4f35f7d1ded1cc6b65bebf8d9b5a70d5f99c0
Mrbysco <Heeybysco@live.nl>
2017-08-13 00:47:34 +0200

    Load after Geolosys

--- src/main/java/com/svennieke/MundaneRedstone/MundaneRedstone.java
+++ src/main/java/com/svennieke/MundaneRedstone/MundaneRedstone.java
@@ -3,7 +3,6 @@ package com.svennieke.MundaneRedstone;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.svennieke.MundaneRedstone.config.MundaneConfigGen;
 import com.svennieke.MundaneRedstone.handler.MundaneGenerationHandler;
 import com.svennieke.MundaneRedstone.init.ModBlocks;
 import com.svennieke.MundaneRedstone.init.ModRecipes;
@@ -11,6 +10,7 @@ import com.svennieke.MundaneRedstone.proxy.CommonProxy;
 import com.svennieke.MundaneRedstone.world.MundaneGeneration;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -23,7 +23,9 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 @Mod(modid = Reference.MOD_ID, 
 	name = Reference.MOD_NAME, 
 	version = Reference.VERSION, 
-	acceptedMinecraftVersions = Reference.ACCEPTED_VERSIONS)
+	acceptedMinecraftVersions = Reference.ACCEPTED_VERSIONS,
+	dependencies = "after:geolosys")
+	
 	
 public class MundaneRedstone {
 	
@@ -34,11 +36,13 @@ public class MundaneRedstone {
 	public static CommonProxy proxy;
 	
 	public static final Logger logger = LogManager.getLogger(Reference.MOD_ID);
+	public static boolean geolosysLoaded = false;
 	
 	@EventHandler
 	public void PreInit(FMLPreInitializationEvent event)
 	{
-		MinecraftForge.EVENT_BUS.register(new MundaneConfigGen());
+		logger.info("Looking if geologys is installed");
+		geolosysLoaded = Loader.isModLoaded("geolosys");
 		
 		logger.info("Initializing Mundane Redstone Ore Block");
 		ModBlocks.init();
@@ -55,7 +59,15 @@ public class MundaneRedstone {
 	@EventHandler
 	public void init(FMLInitializationEvent event)
 	{
-		MinecraftForge.ORE_GEN_BUS.register(new MundaneGenerationHandler());
+		if(MundaneRedstone.geolosysLoaded)
+		{
+			
+		}
+		else
+		{
+			MinecraftForge.ORE_GEN_BUS.register(new MundaneGenerationHandler());
+		}
+		
 		
 		logger.info("Registering World Generation Mundane Redstone Ore");
 		GameRegistry.registerWorldGenerator(new MundaneGeneration(), 1);
