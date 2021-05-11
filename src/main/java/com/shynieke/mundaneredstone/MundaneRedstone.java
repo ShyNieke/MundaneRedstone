73c9fe0afda795adea2389b07666fe49051cfc4f
Mrbysco <Mrbysco@users.noreply.github.com>
2021-05-11 22:01:08 +0200

    Initial port to 1.16.5
    One word: "mixins"
    :)

---
+++ src/main/java/com/shynieke/mundaneredstone/MundaneRedstone.java
@@ -0,0 +1,13 @@
+package com.shynieke.mundaneredstone;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("mundaneredstone")
+public class MundaneRedstone {
+	public static final Logger LOGGER = LogManager.getLogger("mundaneredstone");
+
+	public MundaneRedstone() {
+	}
+}
