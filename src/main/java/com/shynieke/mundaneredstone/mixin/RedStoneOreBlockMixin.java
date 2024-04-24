eaa02f24c4dfd7f0c62221f6ee5d0fa1c4207d0d
Mrbysco <Mrbysco@users.noreply.github.com>
2024-04-24 03:28:57 +0200

    Port to 1.20.5

--- src/main/java/com/shynieke/mundaneredstone/mixin/RedStoneOreBlockMixin.java
+++ src/main/java/com/shynieke/mundaneredstone/mixin/RedStoneOreBlockMixin.java
@@ -3,7 +3,9 @@ package com.shynieke.mundaneredstone.mixin;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RedStoneOreBlock;
@@ -26,9 +28,10 @@ public class RedStoneOreBlockMixin extends Block {
 		info.cancel();
 	}
 
-	@Inject(at = @At("HEAD"), method = "use", cancellable = true)
-	public void use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result, CallbackInfoReturnable<InteractionResult> info) {
-		info.setReturnValue(InteractionResult.PASS);
+	@Inject(at = @At("HEAD"), method = "useItemOn(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/phys/BlockHitResult;)Lnet/minecraft/world/ItemInteractionResult;", cancellable = true)
+	public void use(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand,
+	                BlockHitResult result, CallbackInfoReturnable<ItemInteractionResult> info) {
+		info.setReturnValue(ItemInteractionResult.SUCCESS);
 	}
 
 	@Inject(at = @At("HEAD"), method = "spawnParticles", cancellable = true)
